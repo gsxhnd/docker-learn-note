cef748c5141fdc0d5310dbceaecc4c00b2c95c84
gsxhnd <gsxhnd@gmail.com>
2021-09-26 13:20:11 +0800

    update

--- build.sh
+++
@@ -1,6 +0,0 @@
-rm ./doc/all-in-one.md
-cat ./doc/00-about.md >> ./doc/all-in-one.md
-cat ./doc/01-docker-intro.md >> ./doc/all-in-one.md
-cat ./doc/02-docker-install.md >> ./doc/all-in-one.md
-cat ./doc/03-docker-image.md >> ./doc/all-in-one.md
-cat ./doc/04-docker-container.md >> ./doc/all-in-one.md
